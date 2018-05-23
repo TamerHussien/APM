f825fa069a5ff6db72193c81428a16ea3dec4054
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-05-23 15:05:57 +0200

    m5-05 transforming data with pipes

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -9,6 +9,8 @@ export class ProductListComponent {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
     products: any[] = [
         {
             'productId': 1,
@@ -32,4 +34,8 @@ export class ProductListComponent {
         },
     ];
 
+    toggleImages(): void {
+
+        this.showImage = !this.showImage;
+    }
 }
