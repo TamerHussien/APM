2a2196337e9a6c3266b8ee35c70362e8b563d883
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-05-24 23:31:20 +0200

    m7-02 - Building a Nested Component - Building Nested Components

---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnChanges } from '@angular/core';
+
+@Component({
+    selector: 'pm-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
+})
+
+export class StarComponent implements OnChanges {
+
+    rating: number = 4.5;
+    starWidth: number;
+
+    ngOnChanges () {
+        this.starWidth = this.rating / 86;
+    }
+}
